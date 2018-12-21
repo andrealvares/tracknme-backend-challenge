c7fbc6411b0d253b07c2be559e05fa6270b7eb87
Carlos Henrique <carloshenry.wm@gmail.com>
2018-12-21 21:29:31 -0200

    First test! Up and running. :D

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,10 @@
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class Application {
     public static void main(String[] args) {
+        /* INITIAL SETUP */
+        port(8080);
         get("/", (request, response) -> "Hello world!");
     }
 }
